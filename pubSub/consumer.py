e443b63878edc893f2cdf647e16b448fbb517fdd
DeepThink3r <bielcatao@gmail.com>
2026-02-04 23:13:13 -0300

    Ajuste de comentários

---
+++ pubSub/consumer.py
@@ -0,0 +1,55 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+import select
+import json
+
+load_dotenv()
+
+# Conexão com o banco
+DB_CONFIG = {
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS'),
+    'host': 'localhost',       
+    'port': '5432'
+}
+
+def iniciar_consumidor():
+    conn = psycopg2.connect(**DB_CONFIG)
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cursor = conn.cursor()
+    
+    # Se inscreve no canal que definimos no SQL
+    cursor.execute("LISTEN novas_leituras;")
+
+    try:
+        while True:
+            # O select bloqueia o script até chegar uma notificação (ou timeout de 5s)
+            if select.select([conn], [], [], 5) == ([], [], []):
+                pass
+            else:
+                conn.poll()
+                while conn.notifies:
+                    notify = conn.notifies.pop(0)
+                    
+                    # Converte o payload de texto para JSON Python
+                    dados = json.loads(notify.payload)
+                    
+                    print(f"\n NOTIFICAÇÃO RECEBIDA!")
+                    print(f"   Canal: {notify.channel}")
+                    print(f"   Temp: {dados['temperatura']}°C")
+                    print(f"   Pressão: {dados['pressao']} bar")
+                    
+                    # AQUI entraria sua lógica de alerta
+                    if dados['temperatura'] > 88.0:
+                        print("ALERTA: Temperatura crítica!")
+
+    except KeyboardInterrupt:
+        print("\n Parando consumidor...")
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    iniciar_consumidor()
