97c5d479450042e146826b9d0fa9d9dfb2750dee
DeepThink3r <bielcatao@gmail.com>
2026-02-17 17:15:18 -0300

    Mudança de print para logging no producer e yaml de job acrescentado

--- pubSub/dapr/producer.py
+++ pubSub/dapr/producer.py
@@ -1,16 +1,23 @@
 import os
 import time
+import logging
 import requests # Usamos requests para falar com o Sidecar
 from pyModbusTCP.client import ModbusClient
 
-#Variáveis globais
 
-DAPR_PORT = "3500"
+#Configuração de logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+#Variáveis globais
+DAPR_PORT = os.getenv("DAPR_HTTP_PORT", "3500")
 PUBSUB_NAME = "industrial-pubsub"
 TOPIC_NAME = "leituras_caldeira"
 DAPR_URL = f"http://localhost:{DAPR_PORT}/v1.0/publish/{PUBSUB_NAME}/{TOPIC_NAME}"
 
+# Cliente modbus
 cliente = ModbusClient(host='localhost', port=5020, auto_open=True)
+
 
 def publicar_no_dapr(temp, pressao, vazao):
     payload = {
@@ -22,11 +29,11 @@ def publicar_no_dapr(temp, pressao, vazao):
         # Enviamos para o SIDECAR, não para o banco
         response = requests.post(DAPR_URL, json=payload)
         if response.status_code == 204:
-            print(f"Publicado no Dapr: {payload}")
+            logging.info(f"Publicado no Dapr: {payload}")
         else:
-            print(f"Erro no Dapr: {response.text}")
+            logging.error(f"Erro no Dapr: {response.text}")
     except Exception as e:
-        print(f"Erro de conexão com Sidecar: {e}")
+        logging.error(f"Erro de conexão com Sidecar: {e}")
 
 
 def coletar_e_enviar():
@@ -39,7 +46,8 @@ def coletar_e_enviar():
         # O trabalho do Publisher termina aqui
         publicar_no_dapr(temp_final, pressao_final, vazao_final)
     else:
-        print("Falha na leitura Modbus")
+        logging.error("Falha na leitura Modbus")
+
 
 if __name__ == "__main__":
     try:
